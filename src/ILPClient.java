b7cc6f5a5232f5531036db5da5285e63ef6ccac9
Refactoring <shossain2806@gmail.com>
2018-11-09 05:05:19 +0600

    server-client connection

--- src/ILPClient.java
+++ src/ILPClient.java
@@ -1,22 +1,103 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ILPClient {
-
+	private Socket clientEndPoint;
+	private Scanner sc;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	
 	public ILPClient() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ILPClient client = new ILPClient();
-		try {
-			Socket scktClient = new Socket("127.0.0.1", 6666);
-			System.out.println("Client connected to server: "+scktClient.getRemoteSocketAddress());
-		}catch(IOException e) {
-			System.out.println("Client connection error : "+ e.getLocalizedMessage());
-		}
+		sc = new Scanner(System.in);
 		
 	}
 
+	public void startRunning()  {
+
+		//waiting for a user
+		connectToServer();
+		setupStreams();
+		startMessaging();
+		startListening();
+		
+	}
+	
+	private void connectToServer() {
+		try {
+			
+			clientEndPoint = new Socket("127.0.0.1", 6666);
+			System.out.println("Client connected to server: "+clientEndPoint.getRemoteSocketAddress());
+		}catch(IOException e) {
+			System.out.println("Client connection error : "+ e.getLocalizedMessage());
+		}
+	}
+	
+	private void setupStreams() {
+		try {
+			output = new ObjectOutputStream(clientEndPoint.getOutputStream());
+			output.flush();
+			input = new ObjectInputStream(clientEndPoint.getInputStream());
+		}catch(IOException e) {
+			System.out.println("Stream setup error: "+ e.getLocalizedMessage());
+		}
+		
+	}
+	
+	private void startListening() {
+		String message = "";
+		do {
+			try {
+				 message = (String) input.readObject();
+				 showMessage(message);
+			}catch(ClassNotFoundException e) {
+				System.out.println(e.getLocalizedMessage());
+			}
+			catch (IOException e) {
+				System.out.println(e.getLocalizedMessage());
+			}
+		}while(!message.equals("end"));
+		
+	}
+	
+
+	private void startMessaging() {
+		Runnable r = new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String message = "";
+				do {
+					try {
+						System.out.print("Client: ");
+						message = sc.nextLine();
+						output.writeObject(message);
+						output.flush();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}while(!message.equals("end"));
+			}
+		};
+		new Thread(r).start();
+	}
+	
+	private void showMessage(String message) {
+		System.out.println("Server: "+message);
+	}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ILPClient client = new ILPClient();
+		client.startRunning();
+	}
 }
--- src/ILPServer.java
+++ src/ILPServer.java
@@ -1,49 +1,105 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class ILPServer implements Runnable{
+public class ILPServer {
 	
 	private ServerSocket serverSocket;
-	
+	private Socket client;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private Scanner sc;
 	public ILPServer(int portNo) {
 	
 		try {
-			serverSocket = new ServerSocket(portNo);
-	
+			serverSocket = new ServerSocket(portNo,1);
+			sc = new Scanner(System.in);
+			
 		}catch(IOException e) {
 			System.out.println("Server Initialization error: "+ e.getLocalizedMessage());
 		}
-			
+		
 	}
 	
-	/**
-	 * implemented method of Runnable interface
-	 */
-	public void run() {
-		
-		System.out.println("Server Started");
-		//waiting for new user
-		while(true) {
-			try {
-				System.out.println("Waiting for a connection on port: "+serverSocket.getLocalPort());
-				Socket server = serverSocket.accept();
-				System.out.println("Client connected: "+server.getRemoteSocketAddress());
-			}catch(IOException e) {
-				System.out.println("Server connection error: "+ e.getLocalizedMessage());
-			}
-			
-		}
+	public void startRunning() {
 
+		//waiting for a user
+		try {
+			System.out.println("Waiting for a connection on port: "+serverSocket.getLocalPort());
+		    client = serverSocket.accept();
+		    System.out.println("Client connected: "+client.getRemoteSocketAddress());
+			setupStreams();
+			startMessaging();
+			startListening();
+			
+		}catch(IOException e) {
+			System.out.println("Server connection error: "+ e.getLocalizedMessage());
+		}
 	}
+	
+	private void setupStreams() {
+		try {
+			output = new ObjectOutputStream(client.getOutputStream());
+			output.flush();
+			input = new ObjectInputStream(client.getInputStream());
+		}catch(IOException e) {
+			System.out.println("Stream setup error: "+ e.getLocalizedMessage());
+		}
+		
+	}
+	
+	private void startListening() throws IOException{
+		String message = "";
+		do {
+			try {
+				 message = (String) input.readObject();
+				 showMessage(message);
+			}catch(ClassNotFoundException e) {
+				System.out.println(e.getLocalizedMessage());
+			}
+		}while(!message.equals("end"));
+		
+	}
+	
+
+	private void startMessaging() {
+		Runnable r = new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String message = "";
+				do {
+					try {
+						System.out.print("Server: ");
+						message = sc.nextLine();
+						output.writeObject(message);
+						output.flush();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}while(!message.equals("end"));
+			}
+		};
+		new Thread(r).start();
+	}
+	
+	void showMessage(String message) {
+		System.out.println("Client: "+message);
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ILPServer server = new ILPServer(6666);
-		
-		Thread serverThread = new Thread(server, "Server-Thread");
-		serverThread.start();
-		
+		server.startRunning();
 	}
 
 }
