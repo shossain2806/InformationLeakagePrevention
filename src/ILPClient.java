e692959d711fda19ed382cff3c160cd61a16a7e8
Refactoring <shossain2806@gmail.com>
2018-11-11 00:52:09 +0600

    ..contine

--- src/ILPClient.java
+++ src/ILPClient.java
@@ -1,103 +1,135 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
 import java.util.Scanner;
+
+import javax.crypto.Cipher;
 
 public class ILPClient {
 	private Socket clientEndPoint;
 	private Scanner sc;
-	private ObjectOutputStream output;
-	private ObjectInputStream input;
+	private DataOutputStream output;
+	private DataInputStream input;
+	private PublicKey sharedRSAPublicKey;
+	KeyPair dsaKeypair;
 	
 	public ILPClient() {
 		// TODO Auto-generated constructor stub
 		sc = new Scanner(System.in);
 		
+		try {
+			KeyPairGenerator pairgen = KeyPairGenerator.getInstance("DSA");
+			SecureRandom random = new SecureRandom();
+			pairgen.initialize(512, random);
+			dsaKeypair = pairgen.generateKeyPair();
+		}catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void startRunning()  {
+	public void startRunning() {
 
-		//waiting for a user
+		// waiting for a user
 		connectToServer();
 		setupStreams();
 		startMessaging();
-		startListening();
-		
+
 	}
-	
+
 	private void connectToServer() {
 		try {
-			
+
 			clientEndPoint = new Socket("127.0.0.1", 6666);
-			System.out.println("Client connected to server: "+clientEndPoint.getRemoteSocketAddress());
-		}catch(IOException e) {
-			System.out.println("Client connection error : "+ e.getLocalizedMessage());
+			
+			ObjectInputStream ois = new ObjectInputStream(clientEndPoint.getInputStream());
+			sharedRSAPublicKey = (PublicKey) ois.readObject();
+			
+			ObjectOutputStream oos = new ObjectOutputStream(clientEndPoint.getOutputStream());
+			oos.writeObject(dsaKeypair.getPublic());
+
+			System.out.println("Client connected to server: " + clientEndPoint.getRemoteSocketAddress());
+		} catch (IOException e) {
+			System.out.println("Client connection error : " + e.getLocalizedMessage());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	private void setupStreams() {
 		try {
-			output = new ObjectOutputStream(clientEndPoint.getOutputStream());
-			output.flush();
-			input = new ObjectInputStream(clientEndPoint.getInputStream());
-		}catch(IOException e) {
-			System.out.println("Stream setup error: "+ e.getLocalizedMessage());
+			output = new DataOutputStream(clientEndPoint.getOutputStream());
+			input = new DataInputStream(clientEndPoint.getInputStream());
+		} catch (IOException e) {
+			System.out.println("Stream setup error: " + e.getLocalizedMessage());
 		}
-		
 	}
-	
-	private void startListening() {
-		String message = "";
-		do {
-			try {
-				 message = (String) input.readObject();
-				 showMessage(message);
-			}catch(ClassNotFoundException e) {
-				System.out.println(e.getLocalizedMessage());
-			}
-			catch (IOException e) {
-				System.out.println(e.getLocalizedMessage());
-			}
-		}while(!message.equals("end"));
-		
-	}
-	
 
 	private void startMessaging() {
 		Runnable r = new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				String message = "";
 				do {
+					System.out.print("Client: ");
+					message = sc.nextLine();
+					
 					try {
-						System.out.print("Client: ");
-						message = sc.nextLine();
-						output.writeObject(message);
-						output.flush();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
+						byte[] messageBye = message.getBytes();
+						
+						Signature sign = Signature.getInstance("DSA");
+						sign.initSign(dsaKeypair.getPrivate());
+						
+						sign.update(messageBye);
+				        
+						byte[] signature = sign.sign();
+				         
+						
+						byte[] encrypted = encrypt(sharedRSAPublicKey, messageBye);
+						output.writeInt(encrypted.length);
+						output.write(encrypted);
+						output.writeInt(signature.length);
+						output.write(signature);
+						
+					}catch(Exception e) {
 						e.printStackTrace();
 					}
-				}while(!message.equals("end"));
+				} while (!message.equals("end"));
 			}
 		};
 		new Thread(r).start();
 	}
-	
-	private void showMessage(String message) {
-		System.out.println("Server: "+message);
-	}
-	
 
-	
+	public static byte[] encrypt(PublicKey publicKey, byte[] message) {
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+			return cipher.doFinal(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new byte[0];
+		}
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ILPClient client = new ILPClient();
 		client.startRunning();
 	}
+
 }
--- src/ILPServer.java
+++ src/ILPServer.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -5,97 +7,140 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.crypto.Cipher;
+
 public class ILPServer {
-	
+
 	private ServerSocket serverSocket;
 	private Socket client;
-	private ObjectOutputStream output;
-	private ObjectInputStream input;
+	private DataOutputStream output;
+	private DataInputStream input;
 	private Scanner sc;
+	private KeyPair keyPair;
+	private PublicKey clientDSAPublicKey;
+
 	public ILPServer(int portNo) {
-	
+
 		try {
-			serverSocket = new ServerSocket(portNo,1);
+			serverSocket = new ServerSocket(portNo, 1);
 			sc = new Scanner(System.in);
-			
-		}catch(IOException e) {
-			System.out.println("Server Initialization error: "+ e.getLocalizedMessage());
+			keyPair = buildKeyPair();
+		} catch (IOException e) {
+			System.out.println("Server Initialization error: " + e.getLocalizedMessage());
 		}
-		
+
 	}
-	
+
 	public void startRunning() {
 
-		//waiting for a user
+		// waiting for a user
 		try {
-			System.out.println("Waiting for a connection on port: "+serverSocket.getLocalPort());
-		    client = serverSocket.accept();
-		    System.out.println("Client connected: "+client.getRemoteSocketAddress());
+			System.out.println("Waiting for a connection on port: " + serverSocket.getLocalPort());
+			client = serverSocket.accept();
+
+			ObjectOutputStream objOutput = new ObjectOutputStream(client.getOutputStream());
+			objOutput.writeObject(keyPair.getPublic());
+
+			ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+			clientDSAPublicKey = (PublicKey) ois.readObject();
+
+			System.out.println("Client connected: " + client.getRemoteSocketAddress());
+
 			setupStreams();
-			startMessaging();
 			startListening();
-			
-		}catch(IOException e) {
-			System.out.println("Server connection error: "+ e.getLocalizedMessage());
+
+		} catch (IOException e) {
+			System.out.println("Server connection error: " + e.getLocalizedMessage());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	private void setupStreams() {
+
 		try {
-			output = new ObjectOutputStream(client.getOutputStream());
-			output.flush();
-			input = new ObjectInputStream(client.getInputStream());
-		}catch(IOException e) {
-			System.out.println("Stream setup error: "+ e.getLocalizedMessage());
+			output = new DataOutputStream(client.getOutputStream());
+			input = new DataInputStream(client.getInputStream());
+		} catch (IOException e) {
+			System.out.println("Stream setup error: " + e.getLocalizedMessage());
 		}
-		
+
 	}
-	
-	private void startListening() throws IOException{
-		String message = "";
+
+	private void startListening() {
+
 		do {
 			try {
-				 message = (String) input.readObject();
-				 showMessage(message);
-			}catch(ClassNotFoundException e) {
-				System.out.println(e.getLocalizedMessage());
-			}
-		}while(!message.equals("end"));
-		
-	}
-	
 
-	private void startMessaging() {
-		Runnable r = new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				String message = "";
-				do {
-					try {
-						System.out.print("Server: ");
-						message = sc.nextLine();
-						output.writeObject(message);
-						output.flush();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}while(!message.equals("end"));
+				int len = input.readInt();
+				byte[] encrypted = new byte[len];
+
+				input.readFully(encrypted, 0, len);
+
+				byte[] decrypted = decrypt(keyPair.getPrivate(), encrypted);
+
+				int lenSig = input.readInt();
+				byte[] signature = new byte[lenSig];
+
+				input.readFully(signature, 0, lenSig);
+
+				Signature verifyalg = Signature.getInstance("DSA");
+				verifyalg.initVerify(clientDSAPublicKey);
+
+				verifyalg.update(decrypted);
+				if (verifyalg.verify(signature)) {
+					String message = new String(decrypted);
+					showMessage(message);
+				} else {
+					showMessage("not verified message");
+				}
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
 			}
-		};
-		new Thread(r).start();
+
+		} while (true);
+
 	}
-	
+
 	void showMessage(String message) {
-		System.out.println("Client: "+message);
+		System.out.println("Client: " + message);
 	}
-	
-	
+
+	public static KeyPair buildKeyPair() {
+		try {
+			final int keySize = 2048;
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(keySize);
+			return keyPairGenerator.genKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static byte[] decrypt(PrivateKey privateKey, byte[] encrypted) {
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, privateKey);
+			return cipher.doFinal(encrypted);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new byte[0];
+		}
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ILPServer server = new ILPServer(6666);
